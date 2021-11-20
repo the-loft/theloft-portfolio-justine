7475d743d57961d1a567f5ed3caca82de33cab04
Julien Le Coupanec <julien.le.coupanec@gmail.com>
2021-11-20 18:55:01 +0100

    feat: set up aos

--- components/sections/IdeasSection.vue
+++ components/sections/IdeasSection.vue
@@ -3,17 +3,7 @@
      ************************************************************************* -->
 
 <template>
-  <div>
-    <process-section />
-
-    <base-divider class="my-10 sm:my-16" />
-
-    <benefits-section class="-mt-2" />
-
-    <base-divider class="my-10 sm:my-16" />
-
-    <calendar-section class="-mt-2" />
-  </div>
+  <div></div>
 </template>
 
 <!-- *************************************************************************
@@ -21,12 +11,5 @@
      ************************************************************************* -->
 
 <script>
-// NPM
-import AOS from 'aos'
-
-export default {
-  mounted() {
-    AOS.init({ once: true })
-  },
-}
+export default {}
 </script>
